df214aebd96c7dbc31c12abad50de510e947e8c9
Advaith Kamath <advaithkamath25@gmail.com>
2024-06-20 15:37:46 +0530

    arrays.xml , spinner , listview

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,11 +12,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R.id.itshome
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    //lateinit brake var: Int?
+    var TAG = HomeActivity::class.java.simpleName
+
+    lateinit var mySpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        mySpinner = findViewById(R.id.spinner)
+        mySpinner.onItemSelectedListener = this
+        //brake
         /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -24,6 +34,15 @@ class HomeActivity : AppCompatActivity() {
         Log.i("HomeActivity","data is = "+data)
         val homeTextView:TextView = findViewById(itshome)
         homeTextView.setText(data)  */
+    }
+
+    override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var item = adapter?.selectedItem.toString()
+        Log.i(TAG, item)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 
 
