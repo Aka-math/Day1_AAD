26875a88899eca31951844f2363022739e0f2095
Advaith Kamath <advaithkamath25@gmail.com>
2024-06-20 16:17:22 +0530

    recycler view complete

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -5,24 +5,31 @@ import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.myapplication.R.id.itshome
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     //lateinit brake var: Int?
     var TAG = HomeActivity::class.java.simpleName
 
+    var data = arrayOf("india","english","android","computers")
+
     lateinit var mySpinner: Spinner
+    lateinit var recyclerview: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         mySpinner = findViewById(R.id.spinner)
         mySpinner.onItemSelectedListener = this
+
+        recyclerview = findViewById(R.id.recyclerview)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        var dataAdapter = DataAdapter(data)
+        recyclerview.adapter = dataAdapter
         //brake
         /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
