a940e8dd7df954c24623d9f11e4c248f4ec57b83
Advaith Kamath <advaithkamath25@gmail.com>
2024-06-29 15:11:41 +0530

    notification(?)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.myapplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Build
 import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -27,33 +33,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //chick has hatched  --- activity is visible for clicks
-    override fun onStart() {
-        super.onStart()
-        Log.e(TAG,"activity started -- initialize  data")    }
 
-    //chick has woken up -- come back to the foreground
-    override fun onResume() {
-        super.onResume()
-        Log.w(TAG,"activity resumed --restore state of the app")    }
-
-    //chick has slept  --is partially visible -- background
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG,"activity paused --store the app state")
-    }
-
-    //activity hibernated
-    override fun onStop() {
-        super.onStop()
-        Log.v(TAG,"activity stopped")
-    }
-
-    //all the resources are purged
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG,"activity destroyed")
-    }
 
     fun myClickhandler(view: View) {
         Log.i("MainActivity", "button clicked")
@@ -101,5 +81,44 @@ class MainActivity : AppCompatActivity() {
         //if (intent.resolveActivity(packageManager) != null) {
         startActivity(intent)
         // }
+    }
+
+    fun showNotification(view: View) {
+        createNotificationChannel()
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+
+        var builder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.baseline_account_balance_24)
+            .setContentTitle("vit title")
+            .setContentText("vit text android app dev")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(123,builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "promotions channel name"
+            //getString(R.string.channel_name)
+            val descriptionText = "channel description"
+            //getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
